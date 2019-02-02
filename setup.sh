4bd84ae6e4f8c3d217152c5b3a4804da2464b3a4
Amir Shehata <amir.shehata@gmail.com>
2019-02-02 12:49:53 -0800

    YASP: add setup.sh
    To make it easier to compile added a setup.sh which clones
    the repositories YASP is dependent on.
    
    NOTE: as of this commit the user still needs to resolve any
    dependencies required by the sphinx make system.
    
    Signed-off-by: Amir Shehata <amir.shehata@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+root_dir=$PWD
+install_dir=$PWD/sphinxinstall
+
+# check if pocketsphinx directory doesn't exist.
+# if not then clone it
+[ ! -d pocketsphinx ] && git clone https://github.com/cmusphinx/pocketsphinx.git
+
+# check if sphinxbase directory exists if not, then clone it
+[ ! -d sphinxbase ] && git clone https://github.com/cmusphinx/sphinxbase.git
+
+# create a sphinxinstall directory here.
+[ ! -d sphinxinstall ] && mkdir sphinxinstall
+
+# build sphinxbase
+cd sphinxbase
+./autogen.sh || exit 1
+./configure --prefix=$install_dir || exit 1
+make || exit 1
+make install || exit 1
+cd ..
+
+# build pocketsphinx
+cd pocketsphinx
+./autogen.sh || exit 1
+./configure --prefix=$install_dir || exit 1
+make clean all || exit 1
+make install || exit 1
+cd ..
+
+#build YASP
+export PKG_CONFIG_PATH=$install_dir/lib/pkgconfig/
+
+gcc -Wall -Werror -g -o src/yasp src/yasp.c src/cJSON.c -I $root_dir/pocketsphinx/src/libpocketsphinx/  \
+    -I $root_dir/include -I $root_dir/sphinxbase/include/sphinxbase/ \
+    -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" \
+    `pkg-config --cflags --libs pocketsphinx sphinxbase`
+
