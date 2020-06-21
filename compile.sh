e7cbbcf1c169d99ca3d76f898bae8c9634622761
Amir Shehata <amir.shehata@gmail.com>
2020-06-21 11:13:41 -0700

    YASP: run swig on interface file
    Automatically run swig on yasp.i to generate the python bindings
    
    Signed-off-by: Amir Shehata <amir.shehata@gmail.com>

--- compile.sh
+++ compile.sh
@@ -5,6 +5,7 @@ install_dir=$PWD/sphinxinstall
 
 #build YASP
 export PKG_CONFIG_PATH=$install_dir/lib/pkgconfig/
+swig -python src/yasp.i
 gcc -Wall -Werror -g -o src/yasp src/yasp.c src/cJSON.c -I $root_dir/pocketsphinx/src/libpocketsphinx/  \
     -I $root_dir/include -I $root_dir/sphinxbase/include/sphinxbase/ \
     -DMODELDIR=\"`pkg-config --variable=modeldir pocketsphinx`\" \
